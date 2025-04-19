1bb4be5d62693af6bf2da24dcf3f9cc08215bce6
JuniorNMS <obenee45@gmail.com>
2025-04-19 14:42:25 -0400

    finalized appjs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+// src/App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import TodoList from './components/TodoList';
 import ContactForm from './components/ContactForm';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <NavBar />
-        <Routes>
-          <Route path="/" element={<TodoList />} />
-          <Route path="/todos" element={<TodoList />} />
-          <Route path="/contact" element={<ContactForm />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<TodoList />} />
+            <Route path="/todos" element={<TodoList />} />
+            <Route path="/contact" element={<ContactForm />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
